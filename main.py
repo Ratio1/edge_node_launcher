7be71923fdbc6f448975628c7360897a3b6aecf3
vitalii <vitalii.toderian@gmail.com>
2025-01-09 10:09:32 +0200

    Revert "Load env file on runtime"
    This reverts commit 81849d2231929349c25a63a97f1a732f43d0944e.

--- main.py
+++ main.py
@@ -2,17 +2,9 @@ import sys
 import os
 from PyQt5.QtWidgets import QApplication
 from app_forms.frm_main import EdgeNodeLauncher
-from dotenv import load_dotenv
-from pathlib import Path
 
 # Set the working directory to the location of the executable
 # os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-def load_env():
-    """Load environment variables from the .env file."""
-    dotenv_path = Path(__file__).parent / '.env'
-    load_dotenv(dotenv_path=dotenv_path)
-
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
