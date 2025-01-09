36faae80d6430bc3ed09257a1adcea16dd43b7cd
vitalii <vitalii.toderian@gmail.com>
2025-01-09 10:09:32 +0200

    Revert "Load env file before any imports"
    This reverts commit f01b310f44295cc53d4040fa7a42abcafe71df9f.

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import sys
 import os
+from PyQt5.QtWidgets import QApplication
+from app_forms.frm_main import EdgeNodeLauncher
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Load environment variables should be done before any other in-app imports
-# in order to access env variables in the app.
-dotenv_path = Path(__file__).parent / '.env'
-load_dotenv(dotenv_path=dotenv_path)
-
-from PyQt5.QtWidgets import QApplication
-from app_forms.frm_main import EdgeNodeLauncher
-
 # Set the working directory to the location of the executable
 # os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def load_env():
+    """Load environment variables from the .env file."""
+    dotenv_path = Path(__file__).parent / '.env'
+    load_dotenv(dotenv_path=dotenv_path)
+
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
